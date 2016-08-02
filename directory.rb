0b3f8ca41eb4323522cc5958a2a7892ddda77097
Rosie Allott <rosie.allott@gmail.com>
2016-08-02 11:03:47 +0100

    Added count of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# print list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+#print total number of students
 print "Overall we have"
+#without adding a line character
 print 11
 puts "great students"
