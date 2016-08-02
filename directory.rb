5120f6be8d55e5680bbc100ba580b0785786b384
Rosie Allott <rosie.allott@gmail.com>
2016-08-02 14:29:40 +0100

    Exercises 1. Use each_with_index to print students with number

--- directory.rb
+++ directory.rb
@@ -22,8 +22,8 @@ end
 
 #define printing names method
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
