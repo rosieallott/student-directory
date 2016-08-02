83ff745410ad145adde2cf7ef14b21d62f69511b
Rosie Allott <rosie.allott@gmail.com>
2016-08-02 16:22:25 +0100

    Exercises 6. Center Output

--- directory.rb
+++ directory.rb
@@ -4,15 +4,12 @@ def input_students
   puts "To finish, simply hit return twice"
   #create an empty array
   students = []
-  default_cohort = :november
   name = gets.chomp
   #while name of student is non-zero
   while !name.empty?
     # ask for the cohort
     puts "Please enter cohort of the student #{name}"
-    cohort_input = gets.chomp
-    cohort = (cohort_input == "" ? default_cohort : cohort_input.downcase.to_sym)
-    students << {name: name, cohort: cohort}
+    students << {name: name, cohort: :november}
     puts "Now we have #{students.count} student#{students.count == 1 ? "" : "s"}"
     name = gets.chomp
   end
@@ -21,14 +18,14 @@ end
 
 #define header method
 def print_header
-  puts "The students of Villains Academy"
-  puts "----------"
+  puts "The students of Villains Academy".center(50)
+  puts "----------".center(50)
 end
 
 #define printing names method
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
   end
 end
 
