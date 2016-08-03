fe9ffba004ab58fe7af63d7fba4e861d5e65e599
Rosie Allott <rosie.allott@gmail.com>
2016-08-03 17:14:37 +0100

    Add file saving method

--- directory.rb
+++ directory.rb
@@ -27,6 +27,7 @@ def print_menu
   puts "What would you like to do?"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -42,6 +43,8 @@ def process(selection)
       @students = input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit #end the programme
     else
@@ -65,6 +68,20 @@ end
 #define footer method
 def print_footer
   puts "Overall, we have #{@students.count} great student#{@students.count == 1 ? "" : "s"}"
+  puts
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over array of students
+  @students.each do |student|
+    #make an array of student data then join into a string
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
